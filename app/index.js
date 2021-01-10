7a4e76793612c146bc682714ccbe9662838858ff
watinha <watinha@gmail.com>
2021-01-09 22:27:18 -0300

    logging server errors in the terminal

--- app/index.js
+++ app/index.js
@@ -11,4 +11,9 @@ app.use((req, res) => {
     });
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 module.exports = app;
